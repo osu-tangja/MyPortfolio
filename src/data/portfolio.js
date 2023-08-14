33de76a9b44229b515846ddc5208efe26c6cac98
osu-tangja <63009338+osu-tangja@users.noreply.github.com>
2023-08-14 11:27:42 -0700

    Second Commit

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -3,79 +3,79 @@ export default [
         title: 'Developer Portfolio',
         imgUrl: '/assets/Port.PNG',
         stack: ['HTML', 'Tailwind', 'JavaScript', 'Vite', 'React'],
-        link: ''
+        link: 'https://github.com/osu-tangja/MyPortfolio'
     },
     {
         title: 'Intro to CS',
         imgUrl: '/assets/IntroToCS.PNG',
         stack: ['HTML', 'CSS', 'JavaScript', 'Python', 'C++', 'C#'],
-        link: ''
+        link: 'https://github.com/osu-tangja'
     },
     {
         title: 'Data Structure',
         imgUrl: '/assets/DataStructure+.PNG',
         stack: ['HTML', 'CSS', 'JavaScript', 'Python', 'C++', 'C#'],
-        link: ''
+        link: 'https://github.com/osu-tangja'
     },
     {
         title: 'Web Development',
         imgUrl: '/assets/WebDevelopment.PNG',
         stack: ['HTML', 'CSS', 'JavaScript', 'Python', 'C++', 'C#'],
-        link: ''
+        link: 'https://github.com/osu-tangja'
     },
     {
         title: 'Computer Architecture & Assembly Language',
         imgUrl: '/assets/Assembly.PNG',
         stack: ['HTML', 'CSS', 'JavaScript', 'Python', 'C++', 'C#'],
-        link: ''
+        link: 'https://github.com/osu-tangja'
     },
     {
         title: 'Operating System 1',
         imgUrl: '/assets/OS1.PNG',
         stack: ['HTML', 'CSS', 'JavaScript', 'Python', 'C++', 'C#'],
-        link: ''
+        link: 'https://github.com/osu-tangja'
     },
     {
         title: 'Operating System 2',
         imgUrl: '/assets/OS2.PNG',
         stack: ['HTML', 'CSS', 'JavaScript', 'Python', 'C++', 'C#'],
-        link: ''
+        link: 'https://github.com/osu-tangja'
     },
     {
         title: 'Analysis of Algorithms',
         imgUrl: '/assets/Algorithm.PNG',
         stack: ['HTML', 'CSS', 'JavaScript', 'Python', 'C++', 'C#'],
-        link: ''
+        link: 'https://github.com/osu-tangja'
     },
     {
         title: 'Introduction to Databases',
         imgUrl: '/assets/DataBase.PNG',
         stack: ['HTML', 'CSS', 'JavaScript', 'Python', 'C++', 'C#'],
-        link: ''
+        link: 'https://github.com/osu-tangja'
     },
     {
         title: 'Fundamentals of Software Engineering I&II',
         imgUrl: '/assets/SoftwareEngr.PNG',
         stack: ['HTML', 'CSS', 'JavaScript', 'Python', 'C++', 'C#'],
-        link: ''
+        link: 'https://github.com/osu-tangja'
     },
     {
         title: 'Introduction to Computer Networks',
         imgUrl: '/assets/Networking.PNG',
         stack: ['HTML', 'CSS', 'JavaScript', 'Python', 'C++', 'C#'],
-        link: ''
+        link: 'https://github.com/osu-tangja'
     },
     {
         title: 'Programming Language Fundamentals',
         imgUrl: '/assets/Programming Languaage Fund.PNG',
         stack: ['HTML', 'CSS', 'JavaScript', 'Python', 'C++', 'C#'],
-        link: ''
+        link: 'https://github.com/osu-tangja'
     },
     {
         title: 'Senior Capstone',
         imgUrl: '/assets/Capstone.PNG',
         stack: ['Unity','C#'],
-        link: ''
+        link: 'https://github.com/osu-tangja'
     },
 
 ]
